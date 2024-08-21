7f009f0643c066bfa902735477ebac76943daa99
muhammad <muhammadballak1999@gmail.com>
2024-08-21 17:10:53 +0300

    fix: minor

--- src/app/components/global/Statistics.tsx
+++ src/app/components/global/Statistics.tsx
@@ -10,11 +10,12 @@ import { CountdownTimer } from "./Countdown";
 export const Statistics = () => {
 
   const {
+    isLoading,
     getGameStatus,
     nextClaimTimeSecond,
     rollsLeft,
     profitPerHour,
-    totalCoins
+    totalCoins,
   } = useMainStore();
 
 
@@ -27,35 +28,32 @@ export const Statistics = () => {
   }, [])
 
     return (
-      <div className="flex flex-col gap-2 w-full">
-        <div className="flex gap-2 flex-nowrap items-start w-full">
-        <div className="flex flex-col items-center rounded-md p-2 w-full">
-          <span className="text-[9px] font-semibold">next claim</span>
-          {
-            nextClaimTimeSecond 
-            ?
-             <CountdownTimer seconds={nextClaimTimeSecond} completionText="NOW" onFinish={() => {}} />
-            :
-            <span className="font-bold text-sm">Now</span>
-          
-          }
-        </div>
-        <div className="flex flex-col items-center rounded-md p-2 w-full">
-          <span className="text-[9px] font-semibold">Rolls Left</span>
-          <span className="font-bold text-sm">{ rollsLeft }</span>
-        </div>
-        <div className="flex flex-col items-center rounded-md p-2 w-full relative">
-          <span className="text-[9px] font-semibold">Profit Per Hour</span>
-            <Image src={EmojiSmall} alt="small emoji" className="absolute left-5 top-7" />
-          <span className="font-bold text-sm">
-            { profitPerHour }
-          </span>
-        </div>
-        </div>
-        <div className="flex flex-col items-center rounded-md p-3 w-full relative">
-          <Image src={EmojiMedium} alt="small emoji" className="absolute left-[20%]" />
-          <span className="font-bold text-3xl">{ totalCoins }</span>
-        </div>
-      </div>
+        !isLoading 
+        ?
+          <div className="flex flex-col gap-2 w-full">
+            <div className="flex gap-2 flex-nowrap items-start w-full">
+            <div className="flex flex-col items-center rounded-md p-2 w-full">
+              <span className="text-[9px] font-semibold">next claim</span>
+                <CountdownTimer seconds={nextClaimTimeSecond} completionText="NOW" onFinish={() => {}} />
+            </div>
+            <div className="flex flex-col items-center rounded-md p-2 w-full">
+              <span className="text-[9px] font-semibold">Rolls Left</span>
+              <span className="font-bold text-sm">{ rollsLeft }</span>
+            </div>
+            <div className="flex flex-col items-center rounded-md p-2 w-full relative">
+              <span className="text-[9px] font-semibold">Profit Per Hour</span>
+                <Image src={EmojiSmall} alt="small emoji" className="absolute left-5 top-7" />
+              <span className="font-bold text-sm">
+                { profitPerHour }
+              </span>
+            </div>
+            </div>
+            <div className="flex flex-col items-center rounded-md p-3 w-full relative">
+              <Image src={EmojiMedium} alt="small emoji" className="absolute left-[20%]" />
+              <span className="font-bold text-3xl">{ totalCoins }</span>
+            </div>
+          </div>
+        :
+        <></>
     )
 } 
