8852c27fb76a12310260cc9ce906155ca1723c48
muhammad <muhammadballak1999@gmail.com>
2024-09-01 02:07:43 +0300

    fix: minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
 
   return (
     <>
-      <main className="flex flex-col items-center px-5 pt-[125px] relative z-10">
+      <main className="flex flex-col items-center px-5 pt-[125px] h-full relative z-10">
       <div className="mt-10">
           {
             !isRolled && !isRolling
