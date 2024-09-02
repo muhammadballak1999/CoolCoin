c529f5d0375794e135a762727905e1ba82603dfe
muhammad <muhammadballak1999@gmail.com>
2024-09-02 16:00:36 +0300

    fix: minor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
 
           // @ts-ignore
           setUserData(window.Telegram.WebApp.initDataUnsafe?.user);
-          console.log(userData);
+          console.log(params.get('id'));
           // console.log('user', user);
           verifyUser(initData);
         };
